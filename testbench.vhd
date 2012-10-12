d6213998fbcf9364312593742d7976a0d97bb644
Kevin Kessels <kevinkessels+github@gmail.com>
2012-10-12 20:01:19 +0200

    added a reset to the clock generator

--- testbench.vhd
+++ testbench.vhd
@@ -6,21 +6,24 @@ entity testbench is
 end entity testbench;
 
 architecture STR of testbench is
-	signal data_out : std_logic_vector(7 downto 0);
-	signal data_in  : std_logic_vector(7 downto 0);
-	signal valid    : std_logic;
-	signal start    : std_logic;
-	signal clock    : std_logic;
-	signal reset    : std_logic;
-	constant PERIOD : time := 50 ns;    -- Half the clock period. The frequency will be 1/(2*PERIOD) = 100 MHz
+	signal data_out  : std_logic_vector(7 downto 0);
+	signal data_in   : std_logic_vector(7 downto 0);
+	signal valid     : std_logic;
+	signal start     : std_logic;
+	signal clock     : std_logic;
+	signal reset     : std_logic;
+	constant PERIOD  : time := 50 ns;   -- Half the clock period. The frequency will be 1/(2*PERIOD) = 100 MHz
+	constant RESET_T : time := 500 ns;  -- Hold the reset for 500 ns
 
 begin
 	clock_generator_instance : entity work.clock_generator(BEH)
 		generic map(
-			PERIOD => PERIOD
+			CLOCK_PERIOD => PERIOD,
+			RESET_PERIOD => RESET_T
 		)
 		port map(
-			clock => clock
+			clock => clock,
+			reset => reset
 		);
 
 	dut_instance : entity work.dut(RTL)
@@ -32,7 +35,7 @@ begin
 			clock    => clock,
 			reset    => reset
 		);
-		
+
 	wrapper_instance : entity work.wrapper
 		port map(clk => clock,
 			     rst => reset);
